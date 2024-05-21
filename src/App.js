2a7fca21c31b1f4a4d2db45586231de9adb1021a
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-05-21 05:26:21 +0000

    edit_file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-//import { Component } from "react"; // Don't need anymore.
-import { useState, useEffect } from "react";
+import { useState, useEffect, ChangeEvent } from "react";
 import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
 import "./App.css";
 
+type Monster = {
+  id: string;
+  name: string;
+  email: string;
+};
+
 const App = () => {
-  const [searchField, setSearchField] = useState(""); //[value, setValue]
-  //const [title, setTitle] = useState("");
-  const [monsters, setMonsters] = useState([]);
-  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
+  const [searchField, setSearchField] = useState<string>(""); //[value, setValue]
+  const [monsters, setMonsters] = useState<Monster[]>([]);
+  const [filteredMonsters, setFilteredMonsters] = useState<Monster[]>(monsters);
   console.log("render");
 
   useEffect(() => {
@@ -25,15 +29,10 @@ const App = () => {
     setFilteredMonsters(newFilteredMonsters);
   }, [monsters, searchField]);
 
-  const onSearchChange = (event) => {
+  const onSearchChange = (event: ChangeEvent<HTMLInputElement>): void => {
     const searchFieldString = event.target.value.toLocaleLowerCase();
     setSearchField(searchFieldString);
   };
-
-  // const onTitleChange = (event) => {
-  //   const searchFieldString = event.target.value.toLocaleLowerCase();
-  //   setTitle(searchFieldString);
-  // };
 
   return (
     <div className="App">
@@ -44,67 +43,9 @@ const App = () => {
         className="monsters-search-box"
       />
       <br />
-      {/* <SearchBox
-        onChangeHandler={onTitleChange}
-        placeholder="set title"
-        className="title-search-box"
-      /> */}
       <CardList monsters={filteredMonsters} />
     </div>
   );
 };
-
-// class App extends Component {
-//   constructor() {
-//     super();
-
-//     this.state = {
-//       monsters: [],
-//       searchField: "",
-//     };
-//     console.log("constructor");
-//   }
-
-//   componentDidMount() {
-//     console.log("compoentDidMount");
-//     fetch("https://jsonplaceholder.typicode.com/users")
-//       .then((response) => response.json())
-//       .then((users) =>
-//         this.setState(() => {
-//           return { monsters: users };
-//         })
-//       );
-//   }
-
-//   onSearchChange = (event) => {
-//     const searchField = event.target.value.toLocaleLowerCase();
-//     this.setState(() => {
-//       return { searchField }; //A short hand. The key is going to be the name of variable and value is gonna be value of variable
-//     });
-//   };
-
-//   render() {
-//     const { monsters, searchField } = this.state; //destructured. Do this instead of "this.state"
-//     const { onSearchChange } = this;
-
-//     const filteredMonsters = monsters.filter((monster) => {
-//       console.log(searchField);
-//       return monster.name.toLocaleLowerCase().includes(searchField);
-//     });
-
-//     console.log("render from Appjs");
-//     return (
-//       <div className="App">
-//         <h1 className="app-title">Monsters Rolodex</h1>
-//         <SearchBox
-//           onChangeHandler={onSearchChange}
-//           placeholder="search monsters"
-//           className="monsters-search-box"
-//         />
-//         <CardList monsters={filteredMonsters} />
-//       </div>
-//     );
-//   }
-// }
 
 export default App;
